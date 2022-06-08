3332400e47520f12e3032d640d20cb000a454380
damaris <mdamarisnduku@gmail.com>
2022-06-08 21:59:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function (activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+ function wrapAdjective(param="*"){
+    return function (wrapper="special"){
+        return `You are ${param}${wrapper}${param}!`
+
+    }
+ }
